3ce57aa0b40933faa4ac2c4206af5facc70f9d81
Abderrahmane Abdoullah <ababdoul@e3r7p4.1337.ma>
2024-12-20 18:18:13 +0100

    ls

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-int main()
-{
-    
-}
---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 17:39:05 by ababdoul          #+#    #+#             */
+/*   Updated: 2024/12/18 17:45:07 by ababdoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include </mnt/homes/ababdoul/Desktop/printf/ft_printf.h>
+#include <stdio.h>
+#endif
